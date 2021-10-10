99ac27a586f1f3eef694accd3726833b60b22081
Trevor Anderson <tbone101@gmail.com>
2021-10-09 17:32:29 -0700

    finished

--- Assignments/Understanding_Routing/server.py
+++ Assignments/Understanding_Routing/server.py
@@ -7,11 +7,15 @@ def welcome():
     return "Hello World!"
 
 @app.route("/Dojo")
-def welcome():
+def welcomeDojo():
     return "Dojo"
 @app.route("/say/<name>")
 def say(name):
     return f"Hi {name}"
+@app.route("/repeat/<times>/<word>")
+def repeatWord(times, word =""):
+    times = int(times)
+    return word*times
 
 if __name__ == "__main__":
     app.run(debug=True)
