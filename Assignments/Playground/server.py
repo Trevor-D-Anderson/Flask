1ad16bea28a55cc4f22547df28336af6e5fdfee3
Trevor Anderson <tbone101@gmail.com>
2021-10-09 23:28:10 -0700

    This was interesting

--- Assignments/Playground/server.py
+++ Assignments/Playground/server.py
@@ -7,5 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/play")
+def playBox():
+    # boxes = 3
+    return render_template("index.html", boxes=3)
+
+@app.route("/play/<int:boxes>")
+def playBoxes(boxes):
+    return render_template("index.html", boxes=boxes)
+
+@app.route("/play/<int:boxes>/<color>")
+def playBoxesColor(boxes, color):
+    return render_template("index.html", boxes=boxes, color=color)
+
 if __name__=="__main__":
     app.run(debug=True)
