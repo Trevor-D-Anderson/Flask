0c17664427ad84eaf7686b9308fcba8b8fd1aace
Trevor Anderson <tbone101@gmail.com>
2021-10-13 20:46:38 -0700

    Lots o Changes

--- Assignments/great_number_game/server.py
+++ Assignments/great_number_game/server.py
@@ -1,35 +1,51 @@
 from flask import Flask, render_template, request, redirect, session
+import random
 app = Flask(__name__)  
-app.secret_key = 'my keys!'
+app.secret_key = 'Great Numbers!'
 
-@app.route('/')         
+@app.route('/')
 def index():
-    if 'key_name' and 'total' in session:
-        session['key_name']+=1
+    if 'guess' and 'guesses' in session:
         print('key exists!')
     else:
-        session['key_name'] = 1
-        session['total'] = 0
-        print("key 'key_name' does NOT exist")
-    totalCom = int(session['key_name']) + int(session['total'])
-    return render_template("index.html", totalCom=totalCom)
-
-@app.route('/addTwo', methods=['POST'])
-def addTwo():
-    session['total']+=2
-    print('total works')
-
-    return redirect('/')
+        session['guess'] = random.randrange(1,100)
+        session['guesses'] = []
+        print("key 'guess' was just made")
+    return render_template("index.html")
 
 @app.route('/Reset', methods=['POST'])
 def reset():
     session.clear()
     return redirect('/')
 
-@app.route('/add', methods=['POST'])
-def add():
-    session['total'] += int(request.form['value'])
-    return redirect('/')
+@app.route('/check', methods=['POST'])
+def check():
+    if int(request.form['check']) == session['guess']:
+        print("You Win!")
+        session['value'] = "Win"
+        return redirect('/leaderboard')
+    if len(session['guesses']) >= 4:
+        session['value'] == 'Lose'
+        return redirect('https://www.youtube.com/watch?v=QUzvs6gdrbo&ab_channel=Vidmanheart')
+    elif int(request.form['check']) > session['guess']:
+        print('nope too high')
+        session['value'] = "High"
+        session['guesses'].append(int(request.form['check']))
+        return redirect('/')
+    elif int(request.form['check']) < session['guess']:
+        print('nope too low')
+        session['value'] = "Low"
+        session['guesses'].append(int(request.form['check']))
+        return redirect('/')
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template("leaderboard.html")
+
+@app.route('/leadername')
+def leaderName():
+    
+
 
 if __name__=="__main__":   
     app.run(debug=True)  
